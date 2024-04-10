57816eaf00fada6e3c9b1939342fde882c9d5edb
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-04-10 11:20:53 +0200

    make crud operations with sql alchemy

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
-from models import TodoModel
+#from models import TodoModel
 
 
 app = Flask(__name__)
@@ -12,8 +12,16 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
-db.create_all()
 
+
+class TodoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {'id': self.id, 'task': self.task}
+
+# db.create_all()
 
 TODOS = {
     'todo1': {'task': 'build an API'},
@@ -22,39 +30,50 @@ TODOS = {
 }
 
 parser = reqparse.RequestParser()
-parser.add_argument('task')
+parser.add_argument('task', type='string', help='task of todo, cannot be empty')
 
 def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+    abort(404, message="Todo {} doesn't exist".format(todo_id))
 
 class Todo(Resource):
     def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+        todo = TodoModel.query.get(todo_id)
+        if not todo:
+            abort_if_todo_doesnt_exist(todo_id)
+        return todo.to_dict()
 
     def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
+        todo = TodoModel.query.get(todo_id)
+        if not todo:
+            abort_if_todo_doesnt_exist(todo_id)
+        todo.delete()
+        db.session.commit()
         return '', 204
 
     def put(self, todo_id):
         args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
+        todo = TodoModel.query.get(todo_id)
+        if not todo:
+            abort_if_todo_doesnt_exist(todo_id)
+        todo.task = args['task']
+        db.session.commit()
         return task, 201
 
 
 class TodoList(Resource):
     def get(self):
-        return TodoModel.query.all()
+        todos = [] 
+        for todo in TodoModel.query.all():
+            todos.append(todo.to_dict())
+        return todos
+
 
     def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        todo  = TodoModel(task=args['task'])
+        db.session.add(todo)
+        db.session.commit()
+        return todo.to_dict(), 201
 
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
